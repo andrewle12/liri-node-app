1749f4a398d539f0fc79cb5f769544dcb1eed8a7
Charos12 <andrew.le12@gmail.com>
2018-11-10 23:04:05 -0500

    liri js updates

--- liri.js
+++ liri.js
@@ -1,2 +1,20 @@
 require("dotenv").config();
 var request = require("request");
+
+var command = process.argv[2];
+
+if (command==="concert-this"){
+
+}
+
+if (command==="spotify-this-song"){
+
+}
+
+if (command==="movie-this"){
+
+}
+
+if (command==="do-what-it-says"){
+    
+}
